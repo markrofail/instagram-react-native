c0b4ebbe8a27d6ba2c1879d6ea79d4044f2ec9d4
Mark Rofail <mark@bishop-solutions.com>
2020-03-08 22:49:56 +0200

    fix: remove starter placeholder views

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native'
+
+class Instagram extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Instagram</Text>
+            </View>
+        )
+    }
+}
+
+export default Instagram;
